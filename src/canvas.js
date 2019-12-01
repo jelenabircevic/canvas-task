1cec035ea6d23ae2b5809a9c74253ea22320f9ae
Jelena Bircevic <jelena.bircevic@gmail.com>
2019-12-01 13:22:58 +0100

    Implement rules

--- src/canvas.js
+++ src/canvas.js
@@ -7,17 +7,48 @@ const canvas = new fabric.Canvas('board', {
 const boxSize = 40;
 const boxes = [];
 
+const getPossibleMoves = (x, y) => {
+	const moves = [];
+	moves.push([x + 3, y]);
+	moves.push([x - 3, y]);
+	moves.push([x, y + 3]);
+	moves.push([x, y - 3]);
+	moves.push([x + 2, y + 2]);
+	moves.push([x + 2, y - 2]);
+	moves.push([x - 2, y + 2]);
+	moves.push([x - 2, y - 2]);
+	return moves;
+};
+
 const handleClick = ({ target }) => {
 	const { x, y } = target;
-	const box = boxes[x][y];
+	const activeBox = boxes[x][y];
 	// console.log('you clicked on', x, y);
-	if (!box.used) {
-		box.set('fill', '#908cff'); // not sure why box.fill = '#908cff' didn't work (?)
-		box.used = true;
+	if (!activeBox.used && activeBox.possible) {
+		activeBox.set('fill', '#908cff'); // not sure why box.fill = '#908cff' didn't work (?)
+		activeBox.used = true;
 		// console.log(box);
-		// show possible options
+		// find possible options
+		const possibleMoves = getPossibleMoves(x, y);
+		// console.log(possibleMoves);
+		const validMoves = possibleMoves.filter(([a, b]) => {
+			if (a >= 0 && a < 10 && b >= 0 && b < 10) {
+				return !boxes[a][b].used;
+			}
+			return false;
+		});
+		boxes.forEach((boxGroup) => boxGroup.forEach((box) => {
+			if (!box.used) {
+				box.set('fill', '#ffffff');
+				box.possible = false;
+			}
+		}));
+		validMoves.forEach(([a, b]) => {
+			boxes[a][b].set('fill', '#c3baff');
+			boxes[a][b].possible = true;
+		});
 
-		// canvas.renderAll();
+		canvas.renderAll();
 	} else {
 		// console.log('invalid move!');
 	}
@@ -36,6 +67,7 @@ for (let i = 0; i < 10; i += 1) {
 			stroke: '#eeeeee',
 			x: i,
 			y: j,
+			possible: true,
 		});
 		box.on('mousedown', handleClick);
 		boxes[i].push(box);
